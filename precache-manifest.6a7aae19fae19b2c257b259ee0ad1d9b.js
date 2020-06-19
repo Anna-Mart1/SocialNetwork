150b8223049cfc74caed72b4aec13a7f70f00e6c
MartAnna <ms.mart.anna@gmail.com>
2020-06-19 12:49:49 +0300

    Updates

---
+++ precache-manifest.6a7aae19fae19b2c257b259ee0ad1d9b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8a05216f427e2840e7babbf9e1c1cf2",
+    "url": "/Training-SocialNetwork-/index.html"
+  },
+  {
+    "revision": "30b667a95398f9b05a61",
+    "url": "/Training-SocialNetwork-/static/css/3.f12f7777.chunk.css"
+  },
+  {
+    "revision": "c441a2d7621af0c4fff4",
+    "url": "/Training-SocialNetwork-/static/css/4.7a2aa9bf.chunk.css"
+  },
+  {
+    "revision": "7bac9509bb317a4a04c1",
+    "url": "/Training-SocialNetwork-/static/css/5.0a9de5d5.chunk.css"
+  },
+  {
+    "revision": "2bee230e7c59a1f6feff",
+    "url": "/Training-SocialNetwork-/static/css/main.fb51fca3.chunk.css"
+  },
+  {
+    "revision": "56f296833bffcc80483c",
+    "url": "/Training-SocialNetwork-/static/js/2.b90545a4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Training-SocialNetwork-/static/js/2.b90545a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30b667a95398f9b05a61",
+    "url": "/Training-SocialNetwork-/static/js/3.c7dffd26.chunk.js"
+  },
+  {
+    "revision": "c441a2d7621af0c4fff4",
+    "url": "/Training-SocialNetwork-/static/js/4.515343c7.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/Training-SocialNetwork-/static/js/4.515343c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bac9509bb317a4a04c1",
+    "url": "/Training-SocialNetwork-/static/js/5.8b8903f0.chunk.js"
+  },
+  {
+    "revision": "2bee230e7c59a1f6feff",
+    "url": "/Training-SocialNetwork-/static/js/main.61009c2b.chunk.js"
+  },
+  {
+    "revision": "f0eeb900b0e9446614a4",
+    "url": "/Training-SocialNetwork-/static/js/runtime-main.a9b84cbf.js"
+  },
+  {
+    "revision": "5679d6c18127462763b6f459e6b4ac2b",
+    "url": "/Training-SocialNetwork-/static/media/userPhoto.5679d6c1.jpg"
+  }
+]);
